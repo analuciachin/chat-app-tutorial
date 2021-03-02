bff41394a09af1e7f2407d5f5dc28a56cc37133c
Ana Lucia Chin <ananime@gmail.com>
2021-03-02 15:15:28 -0500

    create helper functions in users.js - server side

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -18,7 +18,12 @@ const Chat = ({ location }) => {
     setRoom(room);
     console.log(socket);
 
-    socket.emit("join", { name, room });
+    socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
   }, [ENDPOINT, location.search]);
 
   return <h1>Chat</h1>;
