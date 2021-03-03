707934ab51e99843b9ed712f9f3c136d0628292f
analuciachin <ananime@gmail.com>
2021-03-03 18:21:42 +0000

    create TextContainer component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,6 +5,7 @@ import io from "socket.io-client";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
 import Messages from "../Messages/Messages";
+import TextContainer from "../TextContainer/TextContainer";
 import "./Chat.css";
 
 let socket;
@@ -12,6 +13,7 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const ENDPOINT = "http://localhost:8080";
@@ -36,6 +38,10 @@ const Chat = ({ location }) => {
   useEffect(() => {
     socket.on("message", (message) => {
       setMessages([...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
     });
   }, [messages]);
 
